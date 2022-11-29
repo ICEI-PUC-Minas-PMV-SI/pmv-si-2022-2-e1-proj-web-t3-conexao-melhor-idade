5c942ed58754aabb1bba5c2842485fb022acffb8
Jose Francisco <jf0516663@gmail.com>
2022-11-28 22:35:04 -0300

    Atualizações no login/prevenção

--- src/jd/dicas-acidentes/script.js
+++ src/jd/dicas-acidentes/script.js
@@ -3,3 +3,21 @@ function mostraMenu() {
     botao.classList.toggle("show");
   
   }
+
+
+
+  const accordionTitles = document.querySelectorAll(".text-2");
+
+  accordionTitles.forEach((accordionTitle) => {
+    accordionTitle.addEventListener("click", () => {
+      if (accordionTitle.classList.contains("is-open")) {
+        accordionTitle.classList.remove("is-open");
+      } else {
+        const accordionTitlesWithIsOpen = document.querySelectorAll(".is-open");
+        accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+          accordionTitleWithIsOpen.classList.remove("is-open");
+        });
+        accordionTitle.classList.add("is-open");
+      }
+    });
+  });
