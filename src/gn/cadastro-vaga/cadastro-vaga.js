8596bc642e239402bea41115e7983289043003a9
Gustavo da silva novaes <112666344+gustavonovaesh@users.noreply.github.com>
2022-12-05 14:46:31 -0300

    atualização 2

---
+++ src/gn/cadastro-vaga/cadastro-vaga.js
@@ -0,0 +1,72 @@
+// declara um conjunto inicial de contatos
+var db_vaga_inicial = {
+    "data": [
+        {
+            "id": 1,
+            "tipo": "Cuidador profissional",
+            "quando": "segunda-feira, manhã",
+            "desc": "ajuda para ajudar na alimentação do dia-a-dia",
+            "exp": "Não",
+        }
+    ]
+}
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var db = JSON.parse(localStorage.getItem('db_contato'));
+if (!db) {
+    db = db_vaga_inicial
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertContato(contato) {
+    // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+    let novoId = 1;
+    if (db.data.length != 0) 
+      novoId = db.data[db.data.length - 1].id + 1;
+    let novoVaga = {
+        "id": novoId,
+        "tipo": contato.nome,
+        "quando" : contato.email,
+        "desc": contato.telefone,
+        "exp" : contato.cidade,
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novoVaga);
+    displayMessage("Contato inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_contato', JSON.stringify(db));
+}
+
+function updateContato(id, contato) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    db.data[index].nome = contato.nome,
+    db.data[index].email = contato.email,
+    db.data[index].telefone = contato.telefone,
+    db.data[index].cidade = contato.cidade,
+    db.data[index].categoria = contato.categoria,
+    db.data[index].website = contato.website
+
+    displayMessage("Contato alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_contato', JSON.stringify(db));
+}
+
+function deleteContato(id) {    
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    displayMessage("Contato removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_contato', JSON.stringify(db));
+}
