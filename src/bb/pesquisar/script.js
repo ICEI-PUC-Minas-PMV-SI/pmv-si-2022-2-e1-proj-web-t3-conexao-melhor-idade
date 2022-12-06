e6e45a08628799ba1545d0e00be0abb3f50207eb
Breno Benfica <109998601+BrenoBenfica@users.noreply.github.com>
2022-12-06 20:24:03 -0300

    Breno Atual

--- src/bb/pesquisar/script.js
+++ src/bb/pesquisar/script.js
@@ -3,14 +3,13 @@ function mostraMenu() {
     botao.classList.toggle("show");
   
   }
-  
 import { conectaApi } from "./conexao-json.js";
   const lista = document.querySelector("[data-lista]");
 
   function constroiCard(avatar, first, last, city, bio, area){
     const pessoa = document.createElement("id");
     pessoa.className = "identifica_pessoa";
-    pessoa.innerHTM = `
+    pessoa.innerHTM += `
                         <div class="caixa-pessoa">
                           <img src="${avatar}" alt="${first},${last}">
                           <div class="edit">
@@ -30,4 +29,5 @@ import { conectaApi } from "./conexao-json.js";
     listaApi.forEach(elemento => lista.appendChild(constroiCard(elemento.avatar, elemento.first, elemento.last, elemento.bio, elemento.area, elemento.city )))
   }
 
-  listaPessoas()
+  console.log (conectaApi)
+  console.log (listaPessoas)
