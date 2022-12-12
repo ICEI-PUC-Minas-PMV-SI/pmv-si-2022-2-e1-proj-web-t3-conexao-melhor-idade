9ccd7c047f5615be98e00c1e4086138b57bc6dd5
Breno Benfica <109998601+BrenoBenfica@users.noreply.github.com>
2022-12-11 22:20:10 -0300

    Breno pesquisas

--- src/bb/busca-candidatos/script.js
+++ src/bb/busca-candidatos/script.js
@@ -1,5 +1,39 @@
 function mostraMenu() {
     const botao = document.querySelector(".menu-mobile");
     botao.classList.toggle("show");
-  
-  }
+}
+
+const url = new URL(location.href);
+const query = url.searchParams.get("q");
+document.getElementById("busca-query").value = query;
+
+
+const users = JSON.parse(localStorage.getItem("users"))
+console.log(users)
+
+function updatePageNumber(pageNumber) {
+    document.getElementById("page-num").innerHTML = pageNumber + 1;
+}
+const maxSize = 10
+const usersFiltrados = users
+.filter(user => !query || user.first.toLowerCase().includes(query.toLowerCase()))
+.slice(0,10)
+
+const containerPessoas = document.getElementById("users") 
+usersFiltrados.forEach((user, index) => {
+    console.log(index,user)  
+    const userHtml = `
+    <a href="bb/perfil-profissional/index.html">
+    <div class="caixa-pessoa">
+        <img src="${user.avatar}" alt="${user.first} ${user.last}">
+        <div class="edit">
+        <h2>${user.first} ${user.last}</h2>
+        <p>${user.city}, ${user.area}.</p>
+        </div>
+        </div>
+        </a>
+        `
+        containerPessoas.innerHTML += userHtml 
+    });
+    
+    
