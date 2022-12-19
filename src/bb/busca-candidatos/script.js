4e6eb708f77c8ad73592a41c877ac6449dcaab39
Marilia Bruzzi <marilia.bruzzi@gmail.com>
2022-12-19 10:07:48 -0500

    fixed links to users profile

--- src/bb/busca-candidatos/script.js
+++ src/bb/busca-candidatos/script.js
@@ -1,39 +1,38 @@
 function mostraMenu() {
-    const botao = document.querySelector(".menu-mobile");
-    botao.classList.toggle("show");
+  const botao = document.querySelector(".menu-mobile");
+  botao.classList.toggle("show");
 }
 
 const url = new URL(location.href);
 const query = url.searchParams.get("q");
 document.getElementById("busca-query").value = query;
 
-
-const users = JSON.parse(localStorage.getItem("users"))
-console.log(users)
+const users = JSON.parse(localStorage.getItem("users"));
+console.log(users);
 
 function updatePageNumber(pageNumber) {
-    document.getElementById("page-num").innerHTML = pageNumber + 1;
+  document.getElementById("page-num").innerHTML = pageNumber + 1;
 }
-const maxSize = 10
+const maxSize = 10;
 const usersFiltrados = users
-.filter(user => !query || user.first.toLowerCase().includes(query.toLowerCase()))
-.slice(0,10)
+  .filter(
+    (user) => !query || user.first.toLowerCase().includes(query.toLowerCase())
+  )
+  .slice(0, 10);
 
-const containerPessoas = document.getElementById("users") 
+const containerPessoas = document.getElementById("users");
 usersFiltrados.forEach((user, index) => {
-    console.log(index,user)  
-    const userHtml = `
-    <a href="bb/perfil-profissional/index.html">
+  console.log(index, user);
+  const userHtml = `
+    <a href="perfil.html?id=${user.id}">
     <div class="caixa-pessoa">
-        <img src="${user.avatar}" alt="${user.first} ${user.last}">
+        <img src="${user.avatar}" alt="${user.first} ">
         <div class="edit">
-        <h2>${user.first} ${user.last}</h2>
+        <h2>${user.first}</h2>
         <p>${user.city}, ${user.area}.</p>
         </div>
         </div>
         </a>
-        `
-        containerPessoas.innerHTML += userHtml 
-    });
-    
-    
+        `;
+  containerPessoas.innerHTML += userHtml;
+});
